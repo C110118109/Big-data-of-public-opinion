7c4ea2ad0ef621faba1f17bdf05e051109d92595
beiting <C110118109@nkust.edu.tw>
2025-04-13 20:29:58 +0800

    first commit

---
+++ website_configs/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+urlpatterns = [
+    # top keywords
+    path('topword/', include('app_top_keyword.urls')),
+    # app top persons
+    path('topperson/', include('app_top_person.urls')),
+    # user keyword analysis
+    path('userkeyword/', include('app_user_keyword.urls')),
+    
+    path('trump/', include('app_voice_trump.urls')),
+
+    # full text search and associated keyword display
+    path('userkeyword_assoc/', include('app_user_keyword_association.urls')),
+
+]
