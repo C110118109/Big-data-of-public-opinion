8821c46d9a6b8866494d9eacc39cde38f52b22e8
beiting <C110118109@nkust.edu.tw>
2025-04-22 12:03:29 +0800

    commit teacher's code(fell)

--- app_user_keyword_sentiment/apps.py
+++ app_user_keyword_sentiment/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class AppUserKeywordSentimentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_user_keyword_sentiment'
